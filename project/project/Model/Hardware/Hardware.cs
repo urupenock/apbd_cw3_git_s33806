67e249c0a3f13e506a09fbeb00a76de239a90090
Anhelina Urupa <s33806@pjwstk.edu.pl>
2026-03-22 14:44:31 +0100

    Add code with auto generated ID

--- project/project/Model/Hardware/Hardware.cs
+++ project/project/Model/Hardware/Hardware.cs
@@ -1,6 +1,13 @@
 ﻿namespace project.Model.Hardware;
 
-public class Hardware
-{
-    
+public abstract class Hardware
+{ public string Id { get; }
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; } = true;
+    public bool IsInService { get; set; } = false;
+    protected Hardware(string name)
+    {
+        Id = "HW-" + Guid.NewGuid().ToString().Substring(0, 5).ToUpper();
+        Name = name;
+    }
 }
