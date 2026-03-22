67e997aeef78fb7a6c20631fcefc62fd4807a3f1
Anhelina Urupa <s33806@pjwstk.edu.pl>
2026-03-22 14:49:44 +0100

    Add code

--- project/project/Model/Users/User.cs
+++ project/project/Model/Users/User.cs
@@ -1,6 +1,14 @@
 ﻿namespace project.Model.Users;
 
-public class User
-{
-    
+public abstract class User
+{ public string Id { get; set; } 
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public abstract int MaxRentalLimit { get; }
+    protected User(string id, string firstName, string lastName)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+    }
 }
