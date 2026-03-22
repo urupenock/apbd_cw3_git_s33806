79c0d6426145a836fb6b74d116f9cde61e24a2e5
Anhelina Urupa <s33806@pjwstk.edu.pl>
2026-03-22 14:54:39 +0100

    Add Rental model to track equip. assignments  and due dates

---
+++ project/project/Model/Rental.cs
@@ -0,0 +1,21 @@
+﻿using project.Model.Hardware; 
+using project.Model.Users;   
+
+namespace project.Model;
+
+public class Rental
+{ public User User { get; }
+    public Hardware.Hardware Equipment { get; }
+    public DateTime RentalDate { get; }
+    public DateTime DueDate { get; }
+    public DateTime? ActualReturnDate { get; set; } 
+
+    public Rental(User user, Hardware.Hardware equipment, int daysLimit)
+    {
+        User = user;
+        Equipment = equipment;
+        RentalDate = DateTime.Now;
+        DueDate = DateTime.Now.AddDays(daysLimit);
+        ActualReturnDate = null; 
+    }
+}
