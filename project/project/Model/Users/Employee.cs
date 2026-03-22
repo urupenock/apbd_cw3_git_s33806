d8e2c14db184cbddabb1330b69b3c93f370d4bdf
Anhelina Urupa <s33806@pjwstk.edu.pl>
2026-03-22 14:51:22 +0100

    Add 2 types with rental limits

---
+++ project/project/Model/Users/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace project.Model.Users;
+
+public class Employee : User
+{
+    public override int MaxRentalLimit => 5;
+    public Employee(string id, string firstName, string lastName) 
+        : base(id, firstName, lastName)
+    {
+    }
+}
---
+++ project/project/Model/Users/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace project.Model.Users;
+
+public class Student : User
+{
+    public override int MaxRentalLimit => 2;
+    public Student(string id, string fName, string lName) : base(id, fName, lName) { }
+}
